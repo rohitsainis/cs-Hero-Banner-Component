cafab61cc9fca2c127f2d1fd2293e18f950cd704
rohitsainis <35720427+rohitsainis@users.noreply.github.com>
2025-12-04 19:59:27 -0800

    Bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+// src/index.ts
 export * from './personalization/PersonalizeProvider';
 export * from './personalization/components/PersonalizedHeroBanner';
 export * from './personalization/hooks/useHeroBanner';
 export * from './personalization/mappers/mapHeroBanner';
+export * from './personalization/resolveVariant';        
+export * from './personalization/personalizeTypes';     // (optional) if you want TravelType type
