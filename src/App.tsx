d28db40b534b8d0e010c7cafe9cce41a2d123e1b
rohitsainis <35720427+rohitsainis@users.noreply.github.com>
2025-12-09 07:52:57 -0800

    Code Format

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import { PersonalizedHeroBanner } from './personalization/components/PersonalizedHeroBanner';
 import { resolveVariant } from './personalization/resolveVariant';
 
-const HERO_CONTENT_TYPE_UID = 'herobanner';
-const FALLBACK_HERO_ENTRY_UID = 'blt299367f0c8134855';
-
-const HERO_EXPERIENCE_ID = 'homepage_hero';
-
 const App: React.FC = () => {
   return (
     <div>
       <PersonalizedHeroBanner
-        experienceId={HERO_EXPERIENCE_ID}
-        contentTypeUid={HERO_CONTENT_TYPE_UID}
-        fallbackEntryUid={FALLBACK_HERO_ENTRY_UID}
-        travelType="Budget" 
+        experienceId={import.meta.env.VITE_CS_HERO_EXPERIENCE_UID}
+        contentTypeUid={import.meta.env.VITE_CS_HERO_CONTENT_TYPE_UID}
+        fallbackEntryUid={import.meta.env.VITE_CS_HERO_FALLBACK_UID}
+        travelType="luxury"
         resolveVariant={resolveVariant}
       />
     </div>
